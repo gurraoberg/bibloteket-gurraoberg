ecc0c52039215e0bb81d2de2b60a47c097abd4a5
gurraoberg <oberg.gustav96@gmail.com>
2023-01-24 12:39:14 +0100

    fixed totalbookvalue

--- src/main/java/se/nackademin/Book.java
+++ src/main/java/se/nackademin/Book.java
@@ -1,10 +1,12 @@
 package se.nackademin;
 
+import java.util.List;
+
 public class Book extends LibraryContent {
 
     public Book(String title, float purchasePrice, int pages) {
         super(title, purchasePrice);
-    }
+    } 
 
     public Book() {}
 
@@ -13,5 +15,21 @@ public class Book extends LibraryContent {
         return newString;
     }
 
+    public void getTotalBookValue(List<String> list) {
+        String[] attributes = null;
+        float totalValue = 0;
+        if (list.size() > 0) {
+            for (String book : list) {
+                attributes = book.split(", ");
+                float price = Float.parseFloat(attributes[1]);
+
+                totalValue += price;
+            }
+            System.out.println("Total value of all our books is " + totalValue + "sek");
+        }
+        else {
+            System.out.println("No data");
+        }
+    }
 
 }
